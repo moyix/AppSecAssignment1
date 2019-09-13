b074fa56012bd776dcd453dc3eb7987eab2628d5
Kevin Gallagher <kevin.gallagher@nyu.edu>
2019-09-12 23:18:17 -0400

    Add promised materials
    In this push I am adding a few materials I
    promised in my most recent announcement. These
    include a Makefile, some pseudocode, another
    functional test, and a skeleton of a third
    functional test.

--- test_main.c
+++ test_main.c
@@ -3,27 +3,49 @@
 #include <stdlib.h>
 
 #define DICTIONARY "wordlist.txt"
+#define TESTDICT "test_worlist.txt"
+
+START_TEST(test_dictionary_normal)
+{
+    hashmap_t hashtable[HASH_SIZE];
+    ck_assert(load_dictionary(TESTDICT, hashtable));
+    // Here we can test if certain words ended up in certain buckets
+    // to ensure that our load_dictionary works as intended. I leave
+    // this as an exercise.
+}
+END_TEST
 
 START_TEST(test_check_word_normal)
 {
     hashmap_t hashtable[HASH_SIZE];
     load_dictionary(DICTIONARY, hashtable);
     const char* correct_word = "Justice";
-    const char* incorrect_word = "Caoimhín";
+    const char* punctuation_word_2 = "pl.ace";
     ck_assert(check_word(correct_word, hashtable));
-    ck_assert(!check_word(incorrect_word, hashtable));
+    ck_assert(!check_word(punctuation_word_2, hashtable));
+    // Test here: What if a word begins and ends with "?
 }
 END_TEST
 
-START_TEST(test_check_word_buffer_overflow)
+START_TEST(test_check_words_normal)
 {
     hashmap_t hashtable[HASH_SIZE];
     load_dictionary(DICTIONARY, hashtable);
-    char incorrect_word[500000];
-    for(int i = 0; i < 499999; i++)
-        incorrect_word[i] = 'A';
-    incorrect_word[499999] = 0;
-    ck_assert(!check_word(incorrect_word, hashtable));
+    char* expected[3];
+    expected[0] = "sogn";
+    expected[1] = "skyn";
+    expected[2] = "betta";
+    char *misspelled[MAX_MISSPELLED];
+    FILE *fp = fopen("test1.txt", "r");
+    int num_misspelled = check_words(fp, hashtable, misspelled);
+    ck_assert(num_misspelled == 3);
+    bool test = strlen(misspelled[0]) == strlen(expected[0]);
+    int len1 = strlen(misspelled[0]);
+    int len2 = strlen(expected[0]);
+    ck_assert_msg(test, "%d!=%d", len1, len2);
+    ck_assert_msg(strcmp(misspelled[0], expected[0]) == 0);
+    ck_assert_msg(strcmp(misspelled[1], expected[1]) == 0);
+    ck_assert_msg(strcmp(misspelled[2], expected[2]) == 0);
 }
 END_TEST
 
@@ -35,7 +57,7 @@ check_word_suite(void)
     suite = suite_create("check_word");
     check_word_case = tcase_create("Core");
     tcase_add_test(check_word_case, test_check_word_normal);
-    tcase_add_test(check_word_case, test_check_word_buffer_overflow);
+    tcase_add_test(check_word_case, test_check_words_normal);
     suite_add_tcase(suite, check_word_case);
 
     return suite;
