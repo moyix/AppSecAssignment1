c3de4dcf03fdb1c0e989bab68a098f10ab868f10
Brendan Dolan-Gavitt <brendandg@gatech.edu>
2019-09-30 16:59:02 -0400

    Fix accidentally committed wrong main.c

--- main.c
+++ main.c
@@ -1,16 +1,30 @@
-#include "dictionary.h"
+#include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <ctype.h>
 
-// test main - DOES NOT NEED TO BE SUBMITTED
-int main(int argc, char ** argv) {
-	if (argc < 3) exit(1);
-	node* hashtable[HASH_SIZE];
-	if (load_dictionary(argv[2], hashtable)) {
-		char * misspelled[MAX_MISSPELLED];
-		printf("%i\n", check_words(fopen(argv[1], "r, ccs=UTF-8"), hashtable, misspelled));
-	}
-	return 0;
+#include "dictionary.h"
+#undef calculate
+#undef getrusage
+#define expected_size 118819
+// default dictionary
+#define DICTIONARY "wordlist.txt"
+#define BODICTIONARY "bo_wordlist.txt"
+#define MAX_MISSPELLED 1000
+int main(int argc, char** argv) {
+    hashmap_t hashtable[HASH_SIZE];
+    if (argc < 3) {
+        fprintf(stderr, "Error: Insufficient arguments!\n");
+        fprintf(stderr, "Usage: ./program to_check.txt wordlist.txt\n");
+        exit(-1);
+    }
+    char read_mode[2] = "r";
+    FILE* fp = fopen(argv[1], read_mode);
+    char * dictionary = argv[2]; 
+    load_dictionary(dictionary, hashtable);
+    char* misspelled[MAX_MISSPELLED];
+    int num_wrong = check_words(fp, hashtable, misspelled);
+    for (int i = 0; i < num_wrong; i++) {
+        printf("%s\n", misspelled[i]);
+    }
 }
